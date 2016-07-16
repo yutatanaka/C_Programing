87f1ff94cca07628886f3179d99e4bf1792fb03a
yutatanaka <koryotennis99@gmail.com>
2016-07-16 23:53:08 +0900

    List 1-9 初期化

---
+++ C言語プログラミング/1-2変数_List1-9.cpp
@@ -0,0 +1,19 @@
+
+/*
+	初期化
+
+	二つの変数に値を代入せずに表示
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int vx, vy;							/* vxとvyはint型の変数 */
+
+	printf("vxの値は%dです。\n", vx);	/* vxの値を表示 */
+	printf("vyの値は%dです。\n", vy);	/* vyの値を表示 */
+
+	getchar();
+	return 0;
+}
