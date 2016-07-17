a2ac9a93726c825ee48848963da36a1fd1469036
yutatanaka <koryotennis99@gmail.com>
2016-07-18 02:47:29 +0900

    非ゼロの判定

---
+++ C言語プログラミング/3-1if文_List3-5.cpp
@@ -0,0 +1,28 @@
+
+/*
+	”ñƒ[ƒ‚Ì”»’è
+
+	“Ç‚İ‚ñ‚¾®”’l‚Íƒ[ƒ‚©‚Ç‚¤‚©
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num;
+
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢G");   scanf("%d", &num);
+
+	if (num)
+	{
+		puts("‚»‚Ì”‚Íƒ[ƒ‚Å‚Í‚ ‚è‚Ü‚¹‚ñB");
+	}
+	else
+	{
+		puts("‚»‚Ì”‚Íƒ[ƒ‚Å‚·B");
+	}
+
+	getchar();
+	return 0;
+}
