7a52833023d36eedc275067e463ccb5a1092b76f
yutatanaka <koryotennis99@gmail.com>
2016-07-18 02:17:28 +0900

    if文/

---
+++ C言語プログラミング/3-1if文_List3-3.cpp
@@ -0,0 +1,29 @@
+
+/*
+	if文・その２
+
+	読み込んだ整数値は５で割り切れないか
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int no;
+
+	printf("整数を入力してください：");
+	scanf("%d", &no);
+
+	if (no % 5)
+	{
+		puts("その数は５で割り切れません。");
+	}
+	else
+	{
+		puts("その数は５で割り切れます。");
+	}
+
+	getchar();
+	return 0;
+}
