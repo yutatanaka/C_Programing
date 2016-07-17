00e57c3e9ec29e65d71373ea77fe0bab5ac902b2
yutatanaka <koryotennis99@gmail.com>
2016-07-18 01:30:20 +0900

    奇数の判定

---
+++ C言語プログラミング/3-1if文_List3-2.cpp
@@ -0,0 +1,24 @@
+
+/*
+	Šï”‚Ì”»’è
+
+	“Ç‚İ‚ñ‚¾®”’l‚ÍŠï”‚©
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int no;
+
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf("%d", &no);
+
+	if (no % 2)
+	{
+		puts("‚»‚Ì”‚ÍŠï”‚Å‚·B");
+	}
+
+	getchar();
+	return 0;
+}
