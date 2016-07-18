58a95bac6a6f12b9f4e47fb1472658010dbc220c
yutatanaka <koryotennis99@gmail.com>
2016-07-18 20:08:32 +0900

    演習3-5

---
+++ C言語プログラミング/演習3-5.cpp
@@ -0,0 +1,33 @@
+
+/*
+	演習3-5
+
+	等価演算子や関係演算子が、1あるいは0の値を生成することを確認するプログラムを作成せよ。
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, num3, min;
+
+	puts("整数1：");   scanf("%d", &num1);
+	puts("整数2：");   scanf("%d", &num2);
+	puts("整数3：");   scanf("%d", &num3);
+
+	min = num1;
+	if (num2 < min)
+	{
+		min = num2;
+	}
+	if (num3 < min)
+	{
+		min = num3;
+	}
+
+	printf("最小値は%dです。\n", min);
+
+	getchar();
+	return 0;
+}
