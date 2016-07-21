4f4c5fb0a23ea19bb7e404ae90bad7e394bc9677
yutatanaka <koryotennis99@gmail.com>
2016-07-22 02:42:17 +0900

    配列

---
+++ C言語プログラミング/5-1配列_List5-1.cpp
@@ -0,0 +1,32 @@
+
+/*
+	配列
+
+	5人の学生の点数を読み込んで合計点と平均点を表示
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int uchida;				/* 内田君の点数 */
+	int satoh;				/* 佐藤君の点数 */
+	int sanaka;				/* 佐中君の点数 */
+	int hiraki;				/* 平木君の点数 */
+	int masaki;				/* 真崎君の点数 */
+	int sum = 0;			/* 合計点 */
+
+	printf("1番：");   scanf("%d", &uchida);  sum += uchida;
+	printf("2番：");   scanf("%d", &satoh);  sum += satoh;
+	printf("3番：");   scanf("%d", &sanaka);  sum += sanaka;
+	printf("4番：");   scanf("%d", &hiraki);  sum += hiraki;
+	printf("5番：");   scanf("%d", &masaki);  sum += masaki;
+
+	printf("合計点：%5d\n", sum);
+	printf("平均点：%5.1f\n", (double)sum / 5);
+
+	getchar();
+	getchar();
+	return 0;
+}
