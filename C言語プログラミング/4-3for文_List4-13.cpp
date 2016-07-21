6ad98c83de855e18e4bb2167ece705f3563dbfee
yutatanaka <koryotennis99@gmail.com>
2016-07-21 15:37:04 +0900

    for文による一定回数の繰返し

---
+++ C言語プログラミング/4-3for文_List4-13.cpp
@@ -0,0 +1,32 @@
+
+/*
+	for文による一定回数の繰返しその２
+
+	指示された個数だけ整数を読み込んで合計値と平均値を表示
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int sum = 0;			/* 合計値 */
+	int num, tmp;
+
+	printf("整数は何個ですか：");
+	scanf("%d", &num);
+
+	for (i = 0; i < num; i++)
+	{
+		printf("No.%d :", i + 1);
+		scanf("%d", &tmp);
+		sum += tmp;
+	}
+
+	printf("合計値：%d\n", sum);
+	printf("平均値：%.2f\n", (double)sum / num);
+
+	getchar();
+	return 0;
+}
