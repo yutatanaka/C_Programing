6404d51983fc4a5f7fb17fa28c0cc75beda7e072
yutatanaka <koryotennis99@gmail.com>
2016-07-25 21:19:27 +0900

    仮引数を受け取らない関数

---
+++ C言語プログラミング/6-2関数の設計_List6-9.cpp
@@ -0,0 +1,55 @@
+
+/*
+	仮引数を受け取らない関数
+
+	読み込んだ正の整数値を逆順に表示
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*  ---正の整数を読み込んで返す--- */
+int scan_pint()
+{
+	int tmp;
+	
+	do{
+		printf("正の整数を入力してください：");
+		scanf("%d", &tmp);
+		if (tmp <= 0)
+		{
+			puts("\a正でない数を入力しないでください。");
+		}
+	} while (tmp <= 0);
+
+	return tmp;
+}
+
+/* ---非負の整数を反転した値を返す--- */
+int rev_int(int num)
+{
+	int tmp = 0;
+
+	if (num > 0)
+	{
+		do{
+			tmp = tmp * 10 + num % 10;
+			num /= 10;
+			printf("num = %d\n", num);
+			printf("tmp = %d\n", tmp);
+		} while (num > 0);
+	}
+	return tmp;
+}
+
+
+int main()
+{
+	int nx = scan_pint();
+
+	printf("反転した値は%dです。\n", rev_int(nx));
+
+	getchar();
+	getchar();
+	return 0;
+}
