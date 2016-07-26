36d3a8c04e6c60342429d190e5370822ea182d40
yutatanaka <koryotennis99@gmail.com>
2016-07-26 20:15:46 +0900

    演習6-14

---
+++ C言語プログラミング/演習6-14.cpp
@@ -0,0 +1,24 @@
+
+/*
+	演習6-14
+
+	静的記憶域期間が与えられたdouble型配列の全要素が0.0で初期化されることを確認するプログラムを作成せよ。
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	static double array[5];
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("array[%d] = %d\n", i, array[i]);
+	}
+
+	getchar();
+	getchar();
+	return 0;
+}
