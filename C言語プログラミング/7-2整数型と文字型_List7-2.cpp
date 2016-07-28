88aef6864919eff47479c68e3842027d743614a0
yutatanaka <koryotennis99@gmail.com>
2016-07-28 14:07:06 +0900

    文字型

---
+++ C言語プログラミング/7-2整数型と文字型_List7-2.cpp
@@ -0,0 +1,22 @@
+
+/*
+	•¶šŒ^
+
+	’P‚È‚écharŒ^‚ª•„†•t‚«Œ^‚©•„†–³‚µŒ^‚©‚ğ”»’è
+*/
+
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	printf("‚±‚Ìˆ—Œn‚ÌcharŒ^‚Í");
+
+	if (CHAR_MIN)
+		puts("•„†•t‚«‚ÌŒ^‚Å‚·B");
+	else
+		puts("•„†–³‚µ‚ÌŒ^‚Å‚·B");
+
+	getchar();
+	return 0;
+}
