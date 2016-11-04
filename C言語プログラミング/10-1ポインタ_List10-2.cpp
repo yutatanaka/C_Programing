a4daf914a17d2825246b71dbde73b02c01b1c574
yutatanaka <koryotennis99@gmail.com>
2016-11-04 16:00:08 +0900

    アドレス演算子

---
+++ C言語プログラミング/10-1ポインタ_List10-2.cpp
@@ -0,0 +1,23 @@
+
+/*
+	アドレスの演算子
+	オブジェクトのアドレスを表示する
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	double x;
+	int a[3];
+
+	printf("n   のアドレス：%p\n", &n);
+	printf("x   のアドレス：%p\n", &x);
+	printf("a[0]のアドレス：%p\n", &a[0]);
+	printf("a[1]のアドレス：%p\n", &a[1]);
+	printf("a[2]のアドレス：%p\n", &a[2]);
+
+	getchar();
+	return 0;
+}
