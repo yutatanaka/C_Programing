37a0545192ca5f52df89759f97ba3d9ac97ba477
yutatanaka <koryotennis99@gmail.com>
2016-11-25 22:12:57 +0900

    点数を入力して点数によって評価を表示する

---
+++ C言語プログラミング/Practis1-4.cpp
@@ -0,0 +1,35 @@
+
+/*
+	点数を入力する。点数が100～80のとき評価を"A"
+	79～60のとき"B"
+	59～0のとき"C"
+	を表示する
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int tensu;
+	while (1)
+	{
+		printf("点数を入力してください：");	scanf("%d", &tensu);
+
+		if (tensu >= 80 && tensu <= 100)
+		{
+			printf("点数:%d 評価:A\n", tensu);
+		}
+		else if (tensu >= 60 && tensu <= 79)
+		{
+			printf("点数:%d 評価:B\n", tensu);
+		}
+		else if (tensu >= 0 && tensu <= 59)
+		{
+			printf("点数:%d 評価:C\n", tensu);
+		}
+	}
+	getchar();
+	return 0;
+}
