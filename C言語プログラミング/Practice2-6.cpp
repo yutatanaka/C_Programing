5099e62bf950a161b10292e7a9507b4d71ac0abe
yutatanaka <koryotennis99@gmail.com>
2016-12-02 16:13:05 +0900

    Practice2-6

---
+++ C言語プログラミング/Practice2-6.cpp
@@ -0,0 +1,35 @@
+
+/* 
+	元金、利率、期間を入力し、元利合計の計算を行い表示する。
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int gankin = 0, ritu = 0, kikan = 0;					/* 元金、利率、期間 */
+	int goukei = 0;											/* 合計 */
+	int i;
+
+	/* 入力処理 */
+	printf("入力してください。\n");
+	printf("元金＝");	scanf("%d", &gankin);
+	printf("利率＝");	scanf("%d", &ritu);
+	printf("期間＝");	scanf("%d", &kikan);
+
+	goukei = gankin;
+
+	/* 期間の回数だけ次の処理を繰り返す */
+	for (i = 0; i < kikan; i++)
+	{
+		goukei = goukei * (100 + ritu) / 100;
+	}
+
+	/* 元利合計を表示する */
+	printf("元利合計＝%d", goukei);
+
+	getchar();
+	getchar();
+	return 0;
+}
