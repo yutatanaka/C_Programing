1a6c370e84bf38a4ba71d0087a203b2d80770141
yutatanaka <koryotennis99@gmail.com>
2017-01-04 19:55:25 +0900

    Practice7-3完成

--- C言語プログラミング/Practice7-3.cpp
+++ C言語プログラミング/Practice7-3.cpp
@@ -7,6 +7,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* 顧客レコード(構造体の宣言) */
 typedef struct
@@ -16,7 +17,7 @@ typedef struct
 	int zandaka;
 }KOKYAKU_M;
 
-KOKYAKU_M krec;
+KOKYAKU_M krec, ktbl[51];	
 
 /* 伝票レコード(構造体の宣言) */
 typedef struct
@@ -33,19 +34,15 @@ int no, max;			/* マスタカウンタ(no)、マスタ件数(max) */
 int shurui, bango;		/* 処理判定、顧客番号 */
 char kakunin[5];
 
-/* 初期画面を表示する関数 */
-void Shoki()
-{
-	printf("          <<<  売上入金データ入力  >>>\n\n");
-	printf("          新規登録＝１、追加登録＝２    :");
-	scanf("%d", &shurui);
-}
+/* ファイルポインタ */
+FILE *fpm;				/* 顧客マスタ用ポインタ */
+FILE *fpd;				/* 伝票用ポインタ */
+
+void Shoki();
+void Touroku();
 
 int main()
 {
-	/* ファイルポインタ */
-	FILE *fpm;				/* 顧客マスタ用ポインタ */
-	FILE *fpd;				/* 伝票用ポインタ */
 
 	/* 初期画面を表示し出力方法を受け取る */
 	Shoki();
@@ -69,12 +66,89 @@ int main()
 
 	/* 顧客マスタファイルを読み込み、noを添え字として、各項目をテーブルに格納する */
 	no = 0;
-	while (fscanf(fpm, "%d %s %d", &krec.bango, krec.namae, &krec.zandaka))
+	while (fread(&krec, sizeof(KOKYAKU_M), 1, fpm))
 	{
-
+		ktbl[no].bango = krec.bango;
+		strcpy(ktbl[no].namae, krec.namae);
+		ktbl[no].zandaka = krec.zandaka;
+		no++;
 	}
 
+	/* データがなくなったらファイルをクローズし、noをmaxとする */
+	fclose(fpm);
+	max = no;
+
+	do
+	{
+		/* 登録関数を実行する */
+		Touroku();
+
+		/* "続けますか?(y/n)"を表示し、確認(kakunin)に結果を受け取る */
+		printf("続けますか？(yes/no)      :");	scanf("%s", kakunin);
+
+		/* 確認が"yes"のときは繰り返す */
+	} while (strcmp(kakunin, "yes") == 0);
 
 	getchar();
 	return 0;
+}
+
+
+/* 初期画面を表示する関数 */
+void Shoki()
+{
+	printf("          <<<  売上入金データ入力  >>>\n\n");
+	printf("          新規登録＝１、追加登録＝２    :");
+	scanf("%d", &shurui);
+}
+
+/* 登録処理をする関数 */
+void Touroku()
+{
+	system("cls");
+
+	/* 顧客番号(bango)を入力する */
+	printf("        1 顧客番号（半角数字4桁）　："); scanf("%d", &bango);
+	printf("        2 顧 客 名                  ：");
+
+	/* 顧客番号でテーブルのサーチを行う */
+	no = 0;
+	while (bango != ktbl[no].bango && no < max)
+	{
+		no++;
+	}
+
+	/* テーブルに顧客番号が存在したら */
+	if (ktbl[no].bango == bango)
+	{
+		/* 顧客名を表示する */
+		printf("%s\n", ktbl[no].namae);
+		/* 伝票区分(kubun)と金額(kingaku)を入力する */
+		printf("        3 伝票区分（売上=1, 入金=2）　：");	scanf("%d", &drec.kubun);
+		printf("        4 金　　額（半角数字８桁）　　：");	scanf("%d", &drec.kingaku);
+
+		/* "ＯＫ？(yes/no)"を表示し、確認(kakunin)を受け取る */
+		printf("        ＯＫ？（yes/no）　　　　　　　：");	scanf("%s", kakunin);
+		/* 確認が"yes"だった場合、伝票ファイルを出力する */
+		/* この時出力方法(shurui)が１のときオープンモードを"w"で２のとき"a"で開く */
+		if (strcmp(kakunin, "yes") == 0)
+		{
+			if (shurui == 1)
+			{
+				fpd = fopen("denpyo_d.txt", "w");
+				shurui = 2;
+			}
+			else
+			{
+				fpd = fopen("denpyo_d.txt", "a");
+			}
+			//fprintf(fpd, "%d %d %d", drec.bango, drec.kubun, drec.kingaku);
+			fwrite(&drec, sizeof(DENPYO_D), 1, fpd);		
+			fclose(fpd);
+		}
+	}
+	else
+	{
+		printf("\n顧客マスタ未登録。確認　OK？(yes)");	scanf("%s", kakunin);
+	}
 }
