c89c0a6a570c581c81b8d4e57b7e906c07f0b17e
yutatanaka <koryotennis99@gmail.com>
2017-01-14 21:58:25 +0900

    文字列7-6

---
+++ C言語プログラミング/文字列7-6.cpp
@@ -0,0 +1,37 @@
+
+/*  
+	文字列を入力し、その文字列の長さ(文字数)を表示するプログラムを作成しなさい
+	＊ 標準関数を使用しないこと
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/* ポインタ変数の宣言 */
+	char *string;
+	int i, len;
+	/* メモリの確保 */
+	string = (char *)malloc(50);
+	
+	/* 入力処理 */
+	printf("文字列を入力：");	scanf("%s", string);
+	
+	i = 0;
+	len = 0;
+	/* 文字列の長さを図る */
+	while (string[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	printf("文字列の長さ：%d", len);
+
+	/* メモリの解放 */
+	free(string);
+	getchar();
+	getchar();
+	return 0;
+}
