df3999aa7c1d630085c083856283926698805bdd
yutatanaka <koryotennis99@gmail.com>
2017-01-19 18:12:24 +0900

    ファイル操作３

--- C言語プログラミング/ファイル操作3.cpp
+++ C言語プログラミング/ファイル操作3.cpp
@@ -4,11 +4,14 @@
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-	char string[60]; 
+	char *string; 
 	char *fileName = "alphabet_bond.txt";
+	long size;
 	FILE *fp;
 
 	/* ファイルが存在しなければ */
@@ -18,14 +21,23 @@ int main()
 		return -1;
 	}
 
+	/* ファイルのサイズを求める */
+	fseek(fp, 0, SEEK_END);
+	size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	string = (char*)malloc(size + 1);
+	memset(string, '\0', size + 1);
+
 	/* ファイルを読み込む */
-	fread(string, 1, 60, fp);
+	fread(string, 1, size, fp);
 	/* ファイルを閉じる */
 	fclose(fp);
 
 	/* 出力 */
 	printf("%s", string);
-
+	
+	free(string);
 	getchar();
 	return 0;
 }
