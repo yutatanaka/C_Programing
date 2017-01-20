1a547425bd2c21c4fe8dcaff367d93bd7c6b0642
yutatanaka <koryotennis99@gmail.com>
2017-01-20 14:59:22 +0900

    課題更新

--- C言語プログラミング/kadai1.cpp
+++ C言語プログラミング/kadai1.cpp
@@ -4,82 +4,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 101277
-
 int main()
 {
-	// ファイル構造体へのポインタを宣言
-	FILE *fp;
+	int i;
+	long size;
+	float vertex[15] = { 0 };
 	char *fileName = "cube.mqo";
-	int tmp = 0;
-	int i = 0;
-	long fileSize;
-	char str[BUFFER_SIZE];
-	char *str2;
-	float value;
-
-	// バッファサイズ分初期化
-	memset(str, '\0', BUFFER_SIZE);
-
-	// ファイルを読み込みモードで開く
-	fp = fopen(fileName, "r");
-
-	// ファイルオープンに失敗した場合
-	if (fp == NULL)
+	char *str, *p, *pp;
+	FILE *fp;
+	
+	/* ファイルが存在しなければエラー処理 */
+	if ((fp = fopen(fileName, "r")) == NULL)
 	{
-		printf("%sを開けませんでした。\n", fileName);
+		printf("ファイルオープン失敗\n");
 		return -1;
 	}
 
-	/*
+	/* ファイルのサイズを求める*/
 	fseek(fp, 0, SEEK_END);
-	fileSize = ftell(fp);
-	char *buffer = new char[fileSize];
+	size = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
-	*/
-
-	while (1)
-	{
-		// 1文字ずつ読み込む
-		tmp = fgetc(fp);
-
-		// ファイルの終端だったらループ終了
-		if (tmp == EOF)
-		{
-			break;
-		}
-
-		// 改行文字だったらなにもしない
-		if (tmp == '\n')
-		{
-			continue;
-		}
-
-		// 文字列バッファへ値を格納
-		str[i++] = (char)tmp;
-	}
-
-	str2 = strstr(str, "vertex 4");
-	str2 += strlen("vertex 4");
-	//printf("%s\n", str2);
-
-	while (*str2 != '}')
-	{
-		if ((*str2 >= '0') && (*str2 <= '9') || (*str2 == '-'))
-		{
-			value = atof(str2);
-			//printf("%f\n", value);
-			strstr(str2, " ");
-			printf("%f\n", value);
-		}
-		++str2;
-	}
-	//printf("%f\n", value);
-
-
-	/* ファイルクローズ */
+	
+	/* サイズを元にファイル分のメモリ確保し、全て初期化 */
+	str = (char*)malloc(size + 1);
+	memset(str, '\0', size + 1);
+	
+	/* ファイルを読み込んだあとファイルを閉じる */
+	fread(str, sizeof(char), size, fp);
 	fclose(fp);
 
+	/* 目当ての文字列の検索を行い、そこまで読み飛ばす */
+	p = strstr(str, "vertex 4 {");
+	p += strlen("vertex 4 {");
+
+	i = 0;
+	while (*p != '}')
+	{
+		if ((*p >= '0') && (*p <= '9') || (*p == '-'))
+		{
+			vertex[i] = atof(p);
+			printf("%f\n", vertex[i]);
+
+			while (*p != ' ')
+			{
+				p++;
+			}
+			i++;
+		}
+		p++;
+	}
+	
+	free(str);
 	getchar();
 	return 0;
 }
