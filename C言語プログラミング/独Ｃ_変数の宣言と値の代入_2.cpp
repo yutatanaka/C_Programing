e3a19b600bc3a9ead4738fab58cb23470ae36d1c
yutatanaka <koryotennis99@gmail.com>
2017-01-25 11:08:27 +0900

    ソースファイルの名前を変更

---
+++ C言語プログラミング/独Ｃ_変数の宣言と値の代入_2.cpp
@@ -0,0 +1,18 @@
+
+/*
+	numという整数型の変数を宣言するプログラムを作成してください。
+	この変数に値1000を代入し、printf()関数を使って画面に次のように表示してください
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int num;
+
+	num = 1000;
+	printf("numは %d\n", num);
+
+	getchar();
+	return 0;
+}
