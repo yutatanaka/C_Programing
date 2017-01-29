3fee00cc36463c389c46899cfaa3b2ec32d1b5cc
yutatanaka <koryotennis99@gmail.com>
2017-01-29 20:19:37 +0900

    練習問題2.4_1

---
+++ C言語プログラミング/独Ｃ_forループ_1.cpp
@@ -0,0 +1,19 @@
+
+/*
+	1から100までの整数を表示するプログラムを作成してください。
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	getchar();
+	return 0;
+}
